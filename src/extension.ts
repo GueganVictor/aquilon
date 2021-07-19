a1eb170778289cd5b4ba19f9438036c53423ab3c
Victor Guegan <victorguegan@orange.fr>
2021-07-19 22:27:23 +0200

    added regex classes to package.json and sorting logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,15 @@
-'use strict';
-
 import * as vscode from 'vscode';
-import { workspace } from 'vscode';
-import { sortAndFormat } from './utils';
+import { parseAndSort } from './utils';
+
 
 console.log('aquilon is starting...');
 
-const config = workspace.getConfiguration();
-const langConfig = config.get('aquilon.classRegex') as any;
-const sortOrder = config.get('aquilon.defaultSortOrder');
-const opt = { prependOrAppendUnknow: false, useWindiCSSGrouping: true, indentLevel: 0 };
+const config = vscode.workspace.getConfiguration();
 
 export function activate(context: vscode.ExtensionContext) {
-
+	
 	let disposable = vscode.commands.registerCommand('aquilon.organizeClasses', () => {
+		const sortOrder = config.get('aquilon.defaultSortOrder');
 
 		let editor = vscode.window.activeTextEditor;
 		if (!editor) { return; }
@@ -31,38 +27,22 @@ export function activate(context: vscode.ExtensionContext) {
 			editor.document.positionAt(0),
 			editor.document.positionAt(editorText.length)
 		);
-		const matchingAttributes = [...editorText.matchAll(langConfig['html'])];
-		matchingAttributes.forEach(attribute => {
-			const currentIndex = (attribute.index || 0) + attribute[0].length - 1 - attribute[1].length - 1;
-			if (attribute[0].includes(':')) {
-				for (let i = currentIndex; i > 0; i--) {
-					if (editorText[i].match('\n')) {
-						opt.indentLevel = currentIndex - i;
-						break;
-					}
-				}
-			}
-			if (!editor) { return; }
-
-			let textRaplce = sortAndFormat(attribute[1], Array.isArray(sortOrder) ? sortOrder : [], opt);
-			newText = newText.replace(attribute[0], "class=\"" + textRaplce + "\"");
-		});
+		newText = parseAndSort(editorText, editor, sortOrder, newText);
 		editor.edit(editBuilder => {
 			editBuilder.replace(range, newText);
 		});
 	});
 
 
-	// if (config.get('aquilon.runOnSave')) {
-	// 	context.subscriptions.push(
-	// 		workspace.onWillSaveTextDocument((_e) => {
-	// 			commands.executeCommand('aquilon.organizeClasses');
-	// 		})
-	// 	);
-	// }
+	if (config.get('aquilon.runOnSave')) {
+		context.subscriptions.push(
+			vscode.workspace.onWillSaveTextDocument((_e) => {
+				vscode.commands.executeCommand('aquilon.organizeClasses');
+			})
+		);
+	}
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,44 @@
+import * as vscode from 'vscode';
+import { workspace } from 'vscode';
+
 type SelectorsDict = { [key: string]: string[]; };
-type Options = { prependOrAppendUnknow: boolean; useWindiCSSGrouping: boolean; indentLevel: number };
-const regexSelector = /([^\s]+):(\([^)]+\)|[^\s]+)/gm;
+type Options = { placeCustomClassesFirst: boolean; useWindiCSSGrouping: boolean; indentLevel: number };
+
+const config = workspace.getConfiguration();
+const langConfig = config.get('aquilon.classRegex') as any;
+const opt : Options = {
+    placeCustomClassesFirst: config.get('aquilon.placeCustomClassesFirst') || false,
+    useWindiCSSGrouping: config.get('aquilon.useWindiCSSGrouping') || true, 
+    indentLevel: 0
+};
+
+const regexSelector = /([^\s]+):\(?([^)]+|[^"'\s]+)[\)"']?/gm;
+
+declare global {
+    interface Array<T> {
+        reIndexOf(rx: string): number;
+    }
+}
 
 export const sortAndFormat = (text: string, sortingArray: string[], options: Options): string => {
 
     const { selectorsArray, baseClasses } = splitBaseClassesAndSelectors(text);
-    baseClasses.sort(function (a, b) {
-        return sortingArray.indexOf(a) - sortingArray.indexOf(b);
+    baseClasses.sort((a: string, b: string): number => {
+        const sortOrder = sortingArray.reIndexOf(a) - sortingArray.reIndexOf(b);
+        if (sortOrder < -2) { console.log(a, b, sortOrder); }
+        return sortOrder;
     });
 
     let formattedString = '';
-    formattedString += baseClasses.join(' ');
+    formattedString += baseClasses.join(' ').replace('(', ' ');
 
     for (const key in selectorsArray) {
-        selectorsArray[key].sort(function (a, b) {
-            return sortingArray.indexOf(a) - sortingArray.indexOf(b);
+        selectorsArray[key].sort((a: string, b: string): number => {
+            return sortingArray.reIndexOf(a) - sortingArray.reIndexOf(b);
         });
         formattedString += '\r\n' + ' '.repeat(options.indentLevel);
         if (options.useWindiCSSGrouping) {
-            formattedString += key + ':(' + selectorsArray[key].join(' ') + ')';
+            formattedString += key + ':(' + selectorsArray[key].map(x => x.trim()).join(' ') + ')';
         } else {
 
             formattedString += selectorsArray[key].map((x) => key + ':' + x).join(' ');
@@ -44,3 +64,38 @@ const splitBaseClassesAndSelectors = (text: string) => {
     const baseClasses = tempBaseClasses.replace(/\s+/g, ' ').trim().split(' ');
     return { selectorsArray, baseClasses };
 };
+
+export const parseAndSort = (editorText: string, editor: vscode.TextEditor | undefined, sortOrder: unknown, newText: string) => {
+    const matchingAttributes = [...editorText.matchAll(langConfig['html'])];
+    matchingAttributes.forEach(attribute => {
+        console.log(attribute);
+        const currentIndex = (attribute.index || 0) + attribute[0].length - 1 - attribute[1].length - 1;
+        if (attribute[0].includes(':')) {
+            for (let i = currentIndex; i > 0; i--) {
+                if (editorText[i].match('\n')) {
+                    opt.indentLevel = currentIndex - i;
+                    break;
+                }
+            }
+        }
+        if (!editor) { return; }
+
+        let textRaplce = sortAndFormat(attribute[1], Array.isArray(sortOrder) ? sortOrder : [], opt);
+        newText = newText.replace(attribute[0], "class=\"" + textRaplce + "\"");
+    });
+    return newText;
+};
+
+
+
+if (!Array.prototype.reIndexOf) {
+    Array.prototype.reIndexOf = function <T>(this: T[], s: string): number {
+        const temp = this as unknown as Array<string>;
+        for (let i = 0; i < temp.length; i++) {
+            if (s.match(new RegExp(temp[i]))) { return i; }
+        }
+
+        if (opt.placeCustomClassesFirst) { return 9999999; }
+        else { return -9999999; }
+    };
+}
